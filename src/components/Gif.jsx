1936dd862a0aa4113d00f68294a6b284df778179
Mayco Barale <maycobarale@gmail.com>
2022-08-23 17:16:58 -0300

    Update img component from chakraUI

--- src/components/Gif.jsx
+++ src/components/Gif.jsx
@@ -1,11 +1,14 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Text, Image } from "@chakra-ui/react";
 import { Link } from "wouter";
 
 const Gif = ({ url, id, title }) => (
   <Box mb={3} p={2} borderWidth="1px" borderRadius="lg" bg="white">
     <Link to={`/gif/${id}`}>
-      <img src={url} alt="" />
-      <Text fontWeight={600}>{title}</Text>
+      {/* <img src={url} alt="" /> */}
+      <Image src={url} alt="" />
+      <Text fontWeight={600} color="blackAlpha.800">
+        {title}
+      </Text>
     </Link>
   </Box>
 );
