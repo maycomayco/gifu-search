3c8e33c89bb287c22929f989ccac55e03bd0ea75
Mayco Barale <maycobarale@gmail.com>
2022-08-23 17:16:46 -0300

    implement Lazy loading

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,19 +1,16 @@
 import { List, ListItem, Link as LinkChakra } from "@chakra-ui/react";
 import { Link } from "wouter";
 
-const Category = ({ name, options = [] }) => (
-  <div>
-    <h3>{name}</h3>
-    <List>
-      {options.map((elem) => (
-        <ListItem key={elem}>
-          <LinkChakra as={Link} to={`/search/${elem}`}>
-            {elem}
-          </LinkChakra>
-        </ListItem>
-      ))}
-    </List>
-  </div>
+const Category = ({ options = [] }) => (
+  <List>
+    {options.map((elem) => (
+      <ListItem key={elem}>
+        <LinkChakra as={Link} to={`/search/${elem}`}>
+          {elem}
+        </LinkChakra>
+      </ListItem>
+    ))}
+  </List>
 );
 
 export default Category;
--- src/components/TrendingSearches/index.jsx
+++ src/components/TrendingSearches/index.jsx
@@ -1,18 +1,30 @@
 import React, { Suspense } from "react";
+import { Spinner } from "@chakra-ui/react";
 import useIsNearScreen from "../../hooks/useIsNearScreen";
 
+/*
+  - React.lazy()
+  - return a promise that resolves to the component to be lazy loaded
+  - is used for lazy loading, is recommended have specifics <Suspense> for each component.
+*/
 const TrendingSearches = React.lazy(() => import("./TrendingSearches"));
 
-/* This component handle de lazy loading of the trending searches */
+/*
+  - Trending searches index component
+  - this component is prioritary beacause it's has the lazy logic to handle the loading of the trending searches component
+*/
 const LazyTrendingSearches = () => {
   const { isNearScreen, fromRef } = useIsNearScreen("100px");
 
   return (
     <div ref={fromRef}>
-      {/* Suspense is used for lazy loading, is recommended have specifics <Suspense> for each component. https://github.com/danilowoz/react-content-loader
-			The fallback parameter receives anythings that can be rendered (components, string, null, etc)
+      {/*
+        - Suspense
+        - is used for lazy loading, is recommended have specifics <Suspense> for each component. https://github.com/danilowoz/react-content-loader
+			  - The fallback parameter receives anythings that can be rendered (components, string, null, etc)
+        - This components show something while the component is loading
 			*/}
-      <Suspense fallback="I am loading...">
+      <Suspense fallback={<Spinner />}>
         {isNearScreen ? <TrendingSearches /> : null}
       </Suspense>
     </div>
