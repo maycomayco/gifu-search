40bb29086299d0341d28e747bf84bdc0cefd6cb4
Mayco Barale <maycobarale@gmail.com>
2022-08-23 17:17:14 -0300

    change layout of gifs columns

--- src/components/ListOfGifs.jsx
+++ src/components/ListOfGifs.jsx
@@ -2,7 +2,7 @@ import { SimpleGrid } from "@chakra-ui/react";
 import Gif from "./Gif";
 
 const ListOfGifs = ({ gifs }) => (
-  <SimpleGrid columns={[2, 4]} spacing={6} minHeight="99vh">
+  <SimpleGrid columns={{ sm: 2, md: 3, lg: 4 }} spacing={6} minHeight="99vh">
     {gifs.map((gif) => (
       <Gif {...gif} key={gif.id} />
     ))}
